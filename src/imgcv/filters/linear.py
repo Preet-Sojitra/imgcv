69580326a52edc7bb925e9531078c8cd043b51e6
Preet Sojitra <preet.dev373@gmail.com>
2024-06-04 12:50:59 +0530

    feat(filters): add more linear filters
    - Add laplacian, sobel, and robert cross filters
    
    - Add a utility function to apply padding and convolution

--- src/imgcv/filters/linear.py
+++ src/imgcv/filters/linear.py
@@ -1,5 +1,6 @@
 import numpy as np
 from imgcv.common import check_image
+from imgcv.filters.utils import apply_convolution
 
 
 def box_filter(img, filter_size):
@@ -20,32 +21,109 @@ def box_filter(img, filter_size):
         raise ValueError("filter_size should be a tuple")
 
     if len(img.shape) == 2:
-
-        pad_height = filter_size[0] // 2
-        pad_width = filter_size[1] // 2
-
-        padded_img = np.pad(
-            img, ((pad_height, pad_height), (pad_width, pad_width)), mode="constant"
-        )
-
         kernel = np.ones(filter_size)
+        final_img = apply_convolution(img, [kernel], seperate=False, take_mean=True)
 
-        # convolve
-        final_img = np.zeros(img.shape)
-        for row in range(img.shape[0]):
-            for col in range(img.shape[1]):
-                window = padded_img[
-                    row : row + filter_size[0], col : col + filter_size[1]
-                ]
-                result = np.mean(window * kernel)
-                result = np.clip(result, 0, 255)
-                final_img[row, col] = np.round(result)
-
-        return final_img.astype(np.uint8)
-
+        return final_img
     else:
         # apply filter to each channel
         final_img = np.zeros(img.shape)
         for i in range(img.shape[2]):
             final_img[:, :, i] = box_filter(img[:, :, i], filter_size)
+        return final_img
+
+
+def laplacian_filter(img, diagonal=False, return_edges=True):
+    """Apply Laplacian filter to the image.
+
+    Args:
+        img (np.ndarray): Input Image array
+        diagonal (bool, optional): If True, apply diagonal laplacian filter. Defaults to False.
+        return_edges (bool, optional): If True, return image with edges detected else return the sharpened image. Defaults to True.
+
+    Raises:
+        ValueError: If diagonal is not a boolean value.
+
+    Returns:
+        np.ndarray: Image with edges detected
+    """
+    check_image(img)
+
+    if not isinstance(diagonal, bool):
+        raise ValueError(f"diagonal should be a boolean value. Got {diagonal}")
+
+    if len(img.shape) == 2:
+        if diagonal:
+            if return_edges:
+                kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+            else:
+                kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        if not diagonal:
+            if return_edges:
+                kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+            else:
+                kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+
+        final_img = apply_convolution(img, [kernel], seperate=False)
+
+        return final_img
+    else:
+        # apply filter to each channel
+        final_img = np.zeros(img.shape)
+        for i in range(img.shape[2]):
+            final_img[:, :, i] = laplacian_filter(img[:, :, i], diagonal, return_edges)
+        return final_img.astype(np.uint8)
+
+
+def robert_cross_filter(img):
+    """Apply Robert Cross filter to the image. This uses 2x2 kernels. Extending this to 3x3 kernel will give us Sobel filter.
+
+    Args:
+        img (np.ndarray): Input Image array
+
+    Returns:
+        np.ndarray: Image with edges detected
+    """
+    check_image(img)
+
+    if len(img.shape) == 2:
+        kernel_x = np.array([[-1, 0], [0, 1]])  # kerenel for x direction
+        kernel_y = np.array([[0, -1], [1, 0]])  # kernel for y direction
+
+        final_img = apply_convolution(img, [kernel_x, kernel_y], seperate=True)
+
+        return final_img
+    else:
+        # apply filter to each channel
+        final_img = np.zeros(img.shape)
+        for i in range(img.shape[2]):
+            final_img[:, :, i] = robert_cross_filter(img[:, :, i])
+        return final_img.astype(np.uint8)
+
+
+def sobel_filter(img):
+    """
+    Apply Sobel filter to the image. This uses 3x3 kernels. This is extension of Robert Cross filter. Center of kerenel is weighted more than the corners. This gives us more accurate edge detection.
+
+    Args:
+        img (np.ndarray): Input Image array
+
+    Returns:
+        np.ndarray: Image with edges detected
+    """
+
+    check_image(img)
+
+    if len(img.shape) == 2:
+        kernel_x = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+        kernel_y = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+
+        final_img = apply_convolution(img, [kernel_x, kernel_y], seperate=True)
+
+        return final_img
+    else:
+        # apply filter to each channel
+        final_img = np.zeros(img.shape)
+        for i in range(img.shape[2]):
+            final_img[:, :, i] = sobel_filter(img[:, :, i])
         return final_img.astype(np.uint8)
